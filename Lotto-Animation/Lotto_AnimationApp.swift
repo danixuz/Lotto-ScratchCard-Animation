0fc24263399af9de8166c9efe8bb9ded9e3909cd
danixuz <danixuz@icloud.com>
2022-07-26 21:50:36 +0300

    Initial Commit

---
+++ Lotto-Animation/Lotto_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lotto_AnimationApp.swift
+//  Lotto-Animation
+//
+//  Created by Daniel Spalek on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Lotto_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
